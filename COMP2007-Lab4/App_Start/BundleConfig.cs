46f27f700f0f9fb38124a38fd63ca638e1a11d2c
Wangzhetuteng <ericdicaprio@outlook.com>
2016-07-12 08:17:59 -0400

    Initial Commit

--- COMP2007-Lab4/App_Start/BundleConfig.cs
+++ COMP2007-Lab4/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace COMP2007_Lab4
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
--- COMP2007-Lab4/Controllers/HomeController.cs
+++ COMP2007-Lab4/Controllers/HomeController.cs
@@ -8,18 +8,24 @@ namespace COMP2007_Lab4.Controllers
 {
     public class HomeController : Controller
     {
+        public string SubTitle;
+
+        //GET: /Home/Index or /
         public ActionResult Index()
         {
+            this.SubTitle = "Hello World!";
             return View();
         }
 
+        //GET: Home/About
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return View(SubTitle);
         }
 
+        //GET: Home/Contact
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
